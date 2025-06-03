77da95cc690da584193a8f435e1ec91b464f389f
Essie-chestter <estherchestter@gmail.com>
2025-06-03 15:49:03 +0300

    Create app.tsx

---
+++ app.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { PentestSimulation } from './components/PentestSimulation';
+
+function App() {
+  return (
+    <div className="App">
+      <PentestSimulation />
+    </div>
+  );
+}
+
+export default App;
